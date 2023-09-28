1747efeebab73e8826e88fade896bff63b854c94
Mengzhen Chen <mengzhenchen2@gmail.com>
2023-09-28 13:55:25 +1000

    1903. Largest Odd Number in String

---
+++ greedy/LongestOddInString.js
@@ -0,0 +1,17 @@
+// 1903. Largest Odd Number in String
+/**
+ * @param {string} num
+ * @return {string}
+ */
+var largestOddNumber = function (num) {
+  const numLen = num.length;
+  let i = 0;
+  for (j in num) {
+    i = numLen - j - 1;
+    let intNum = parseInt(num[i]);
+    if (intNum % 2 != 0) {
+      return num.substring(0, i + 1);
+    }
+  }
+  return "";
+};
